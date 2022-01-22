e3ff59947ef4b764975fec9fb4e35318016a88f5
Maxwell Carlson <bart4282819@gmail.com>
2022-01-21 21:54:09 -0800

    Why is single and double layer network identical loss?

--- FashionMNIST/FashionMNIST.py
+++ FashionMNIST/FashionMNIST.py
@@ -1,7 +1,15 @@
-import numpy
 import torch
 import torchvision
+import numpy as np
 from torchvision.transforms import ToTensor
+from torchsummary import summary
+import matplotlib.pyplot as plt
+
+if torch.cuda.is_available():  
+    dev = "cuda:0" 
+else:  
+    dev = "cpu"  
+device = torch.device(dev)  
 
 # datasets
 trainset = torchvision.datasets.FashionMNIST('./data',
@@ -9,6 +17,7 @@ trainset = torchvision.datasets.FashionMNIST('./data',
     train=True,
     transform=ToTensor()
     )
+
 testset = torchvision.datasets.FashionMNIST('./data',
     download=True,
     train=False,
@@ -23,10 +32,10 @@ lr          = 0.001
 
 # dataloaders
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchSize,
- shuffle=True, num_workers=2)
+ shuffle=True, num_workers=4)
 
 testloader = torch.utils.data.DataLoader(testset, batch_size=batchSize,
- shuffle=False, num_workers=2)
+ shuffle=False, num_workers=4)
 
 # constant for classes
 classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
@@ -37,25 +46,59 @@ classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 model = torch.nn.Sequential(
     torch.nn.Linear(InputSize, Neurons),
     torch.nn.ReLU(),
+    torch.nn.Linear(Neurons, Neurons),
+    torch.nn.ReLU(),
     torch.nn.Linear(Neurons, len(classes))
     )
+
+model = model.to(device)
+#summary(model, (batchSize, InputSize, Neurons))
 
 loss_fn = torch.nn.CrossEntropyLoss()
 optim = torch.optim.SGD(model.parameters(), lr=lr, momentum=0)
 
 if __name__ == '__main__':
 
+    accPerEpoch = []
+
     for e in range(epochs):
-        for features, target in trainloader:
+        def run(loader, train):
+            acc = 0
+            for i, (features, target) in enumerate(loader):
     
-            a = features.numpy()
-            x = torch.reshape(features, (30, 784))
-            a = x.numpy()
+                features = features.to(device)
+                target = target.to(device)
+                x = torch.reshape(features, (len(target), 784))
+                pred = model(x)
 
-            pred = model(x)
+                # Calculate number of correct values
+                #maxVals, maxIdx = torch.max(pred, 1)
+                #acc += (maxIdx == target).sum().item()
 
-            optim.zero_grad()
-            loss = loss_fn(pred, target)
-            loss.backward()
-            optim.step()
+                if train:
+                    optim.zero_grad()
+                    loss = loss_fn(pred, target)
+                    if i % 100 == 0:
+                        print(f'Epoch {e+1}, loss: {loss.item()}')
+                    loss.backward()
+                    optim.step()
+                else:
+                    # Calculate number of correct values
+                    maxVals, maxIdx = torch.max(pred, 1)
+                    acc += (maxIdx == target).sum().item()
+            return acc
+            
+        run(trainloader, train=True)
+        acc = run(testloader, train=False)
+
+        acc /= len(testset)
+        print(f'Accuracy for Epoch {e+1}: {acc}')
+        accPerEpoch.append(acc)
+
+    plt.plot(range(len(accPerEpoch)), accPerEpoch)
+    plt.title('Accuracy vs. Epoch, 2 x 1024 FC layer')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.show()
+
             
